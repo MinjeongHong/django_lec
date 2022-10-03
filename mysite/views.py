2fa7fecca7ab7968f49b956f423d42b971e460ff
MinjeongHong <einstein2222@naver.com>
2022-10-03 14:48:24 +0900

    3주차 django 개발 시작

--- mysite/views.py
+++ mysite/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import MainContent
 
 def index(request):
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list': content_list}
+    return render(request, 'mysite/content_list.html', context)
 
-    content_list=MainContent.objects.order_by('-pub_date')
-    context={'content_list':content_list}
-    return render(request,'mysite/content_list.html',context)
+def detail(request, content_id):
+    content_list = MainContent.objects.get(id=content_id)
+    context = {'content_list': content_list}
+    return render(request, 'mysite/content_detail.html', context)
