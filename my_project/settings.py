277610527a9850fa77e345b1b1bae7bc5ad4c0f6
MinjeongHong <einstein2222@naver.com>
2022-10-10 17:20:06 +0900

    4주차 django 개발 시작

--- my_project/settings.py
+++ my_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mysite',
-    'pages'
+    'pages',
     'accounts.apps.AccountsConfig',
 ]
 
@@ -113,13 +114,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static')
+    #BASE_DIR / 'static',
 ]
 
 # Default primary key field type
--- my_project/urls.py
+++ my_project/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
+#이미지를 위해 추가한 코드
+#from main.views import index
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mysite/', include('mysite.urls')),
     path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
 ]
+
+#이미지를 위해 추가한 코드2
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
